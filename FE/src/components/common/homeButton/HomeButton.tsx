b298c1f90c8dbb3b509ed6bf5279412e0a4831e9
hosup2 <dosl196122@naver.com>
2026-01-22 16:27:34 +0900

    fix: SidebarStyleFix
    사이드 바 스타일 수정, 테마 변경 토글 아이콘 수정, 테스트용 홈버튼 추가

---
+++ FE/src/components/common/homeButton/HomeButton.tsx
@@ -0,0 +1,26 @@
+// src/components/common/homeButton/HomeButton.tsx
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Home } from 'react-feather';
+import './HomeButton.css';
+
+interface HomeButtonProps {
+    className?: string;
+}
+
+const HomeButton: React.FC<HomeButtonProps> = ({ className = '' }) => {
+    const navigate = useNavigate();
+
+    return (
+        <button
+            className={`home-button ${className}`}
+            onClick={() => navigate('/home')}
+            title="홈으로 이동"
+        >
+            <Home />
+
+        </button>
+    );
+};
+
+export default HomeButton;
--- FE/src/pages/Login.tsx
+++ FE/src/pages/Login.tsx
@@ -1,7 +1,7 @@
 //로그인 페이지
 
 import React from 'react';
-
+import HomeButton from '../components/common/homeButton/HomeButton';
 /**
  * 각 페이지 컴포넌트
  * App.tsx에서 이미 Header와 Sidebar를 감싸고 있으므로,
@@ -13,6 +13,10 @@ const Login: React.FC = () => {
             {/* 이 안의 내용이 App.tsx의 <main> 태그 안에 렌더링됩니다. */}
             <h2>로그인</h2>
             <p>실시간 협업 에디터 영역입니다.</p>
+            {/* HomeButton 컴포넌트를 사용하여 홈으로 이동하는 버튼을 추가 */}
+            <div style={{ marginTop: '24px' }}>
+                <HomeButton />
+            </div>
         </div>
     );
 };
