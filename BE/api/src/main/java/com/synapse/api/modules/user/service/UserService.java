c62cd1e4149a5965906026937ecb5c0d089846c0
srcho01 <seorim5306@gmail.com>
2026-01-26 22:44:46 +0900

    feat: implement user login with OAuth

---
+++ BE/api/src/main/java/com/synapse/api/modules/user/service/UserService.java
@@ -0,0 +1,89 @@
+package com.synapse.api.modules.user.service;
+
+import com.synapse.api.modules.user.dto.oauth.OAuthUserInfo;
+import com.synapse.api.modules.user.dto.request.LoginRequest;
+import com.synapse.api.modules.user.dto.response.LoginResult;
+import com.synapse.api.modules.user.entity.OAuthAccount;
+import com.synapse.api.modules.user.entity.User;
+import com.synapse.api.modules.user.repository.OAuthRepository;
+import com.synapse.api.modules.user.repository.UserRepository;
+import com.synapse.api.util.exception.BusinessException;
+import com.synapse.api.util.response.ErrorCode;
+import com.synapse.api.util.redis.TokenRedisService;
+import com.synapse.api.util.security.JwtUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final OAuthRepository oAuthRepository;
+    private final OAuthServiceFactory oAuthServiceFactory;
+    private final TokenRedisService tokenRedisService;
+    private final JwtUtil jwtUtil;
+
+    @Transactional
+    public LoginResult login(LoginRequest request) {
+
+        OAuthUserInfo oAuthUserInfo = getOAuthUserInfo(request);
+
+        Optional<OAuthAccount> optionalOAuth = oAuthRepository.findByProviderIdAndProvider(
+                oAuthUserInfo.getProviderId(),
+                oAuthUserInfo.getProvider()
+        );
+
+        UUID id;
+        if (optionalOAuth.isPresent()) {
+            OAuthAccount oAuth = optionalOAuth.get();
+            User user = oAuth.getUser();
+
+            // 탈퇴(soft delete)된 계정인지 체크
+            if (oAuth.getDeletedAt() != null || user.getDeletedAt() != null) {
+                throw new BusinessException(ErrorCode.USER_NOT_FOUND);
+            }
+
+            id = user.getId();
+        } else {
+            // 가입하지 않은 유저 -> 신규 회원가입
+            id = saveNewUser(oAuthUserInfo);
+        }
+
+        String access = jwtUtil.generateAccessToken(id);
+        String refresh = tokenRedisService.generateRefreshToken(id);
+        return LoginResult.builder()
+                .accessToken(access)
+                .refreshToken(refresh)
+                .build();
+    }
+
+    private OAuthUserInfo getOAuthUserInfo(LoginRequest request) {
+        OAuthService oAuthService = oAuthServiceFactory.getService(request.provider());
+        return oAuthService.getUserInfo(request.accessToken());
+    }
+
+    private UUID saveNewUser(OAuthUserInfo oAuthUserInfo) {
+        User user = userRepository.save(User.builder()
+                .email(oAuthUserInfo.getEmail())
+                .name("temp")
+                .build()
+        );
+
+        System.out.println(oAuthUserInfo.getEmail());
+
+        oAuthRepository.save(OAuthAccount.builder()
+                .provider(oAuthUserInfo.getProvider())
+                .providerId(oAuthUserInfo.getProviderId())
+                .user(user)
+                .build()
+        );
+
+        return user.getId();
+    }
+
+}
