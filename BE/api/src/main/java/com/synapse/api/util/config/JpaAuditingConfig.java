82f055ea168772257fc52cfbd03c0d971c56f556
srcho01 <seorim5306@gmail.com>
2026-01-26 22:39:37 +0900

    feat: add configurations

---
+++ BE/api/src/main/java/com/synapse/api/util/config/JpaAuditingConfig.java
@@ -0,0 +1,9 @@
+package com.synapse.api.util.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaAuditingConfig {
+}
