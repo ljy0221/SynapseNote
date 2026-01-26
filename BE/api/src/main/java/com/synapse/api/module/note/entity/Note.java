90c08bc9800871ee8d471f6845b59b25925a0eb0
woduddl1000 <woduddl1000@gmail.com>
2026-01-26 13:23:54 +0900

    feat/#40-UUID_v7_Generator
    - 엔티티 수정

--- BE/api/src/main/java/com/synapse/api/module/note/entity/Note.java
+++ BE/api/src/main/java/com/synapse/api/module/note/entity/Note.java
@@ -2,12 +2,14 @@ package com.synapse.api.module.note.entity;
 
 import com.synapse.api.module.user.entity.User;
 import com.synapse.api.util.entity.BaseEntity;
+import com.synapse.api.util.generator.UuidV7Generator;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
+import org.hibernate.annotations.GenericGenerator;
 
 import java.util.UUID;
 
@@ -23,7 +25,11 @@ import java.util.UUID;
 public class Note extends BaseEntity {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.UUID)
+    @GeneratedValue(generator = "uuid-v7")
+    @GenericGenerator(
+        name = "uuid-v7",
+        type = UuidV7Generator.class
+    )
     @Column(columnDefinition = "uuid")
     private UUID id;
 
