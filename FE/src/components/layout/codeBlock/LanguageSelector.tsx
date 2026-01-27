9af7b18d04c752aafc8752c07331a34e1c0d607f
woduddl1000 <woduddl1000@gmail.com>
2026-01-27 09:31:58 +0900

    feat/#51-multi_language_support
    - 언어 선택 UI 구현
    - javascript, java 실행 구현

---
+++ FE/src/components/layout/codeBlock/LanguageSelector.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import type { Language } from '../../../types/execution/ExecutionTypes';
+import './LanguageSelector.css';
+
+interface LanguageSelectorProps {
+  value: Language;
+  onChange: (value: Language) => void;
+  disabled?: boolean;
+}
+
+export const LanguageSelector: React.FC<LanguageSelectorProps> = ({ value, onChange, disabled }) => {
+  return (
+    <select
+      className="language-selector"
+      value={value}
+      onChange={(e) => onChange(e.target.value as Language)}
+      disabled={disabled}
+    >
+      <option value="python">Python 3.11</option>
+      <option value="javascript">JavaScript (Node 20)</option>
+      <option value="java">Java 17</option>
+    </select>
+  );
+};
