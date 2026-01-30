c203dad133367a9b403f26c6d536bc860dffb483
hosup2 <dosl196122@naver.com>
2026-01-30 10:24:05 +0900

    feat: Remove Mockup Data
    목업 데이터 삭제 후 API 응답 데이터 연결

--- FE/src/api/bookmark/Bookmarks.adapter.ts
+++ FE/src/api/bookmark/Bookmarks.adapter.ts
@@ -1,11 +1,10 @@
 // src/api/bookmark/Bookmarks.adapter.ts
-import type { ApiResponse } from '../../types/common/apiResponse';
 import { GetBookmarksResponse } from '../../types/bookmark/BookmarkResponse';
 
 export const adaptBookmarkIds = (
-  res: ApiResponse<GetBookmarksResponse>
+  res: GetBookmarksResponse
 ): Set<string> => {
   return new Set(
-    res.data.content.map(note => note.id)
+    res.content.map(note => note.id)
   );
 };
