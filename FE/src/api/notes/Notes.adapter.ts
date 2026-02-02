0094ccb357886fc2c6914e7a5cd8f3608934974e
Tyler-1102 <7176ryu@naver.com>
2026-02-02 12:35:15 +0900

    feat/#123-FrontDateInfoTimeStamp
    프론트 날짜정보 timestamp 변경 구현

--- FE/src/api/notes/Notes.adapter.ts
+++ FE/src/api/notes/Notes.adapter.ts
@@ -1,9 +1,9 @@
 import type { GetNotesResponse } from '../../types/note/GetNotes';
 import type { NoteListItem } from '../../types/note/GetNotes';
+import dayjs from 'dayjs';
 
-export const adaptNotesForSidebar = (
-  res: GetNotesResponse
-): NoteListItem[] => {
+export const adaptNotesForSidebar = (res: GetNotesResponse): NoteListItem[] => {
+  console.log('변환 전:', res.content[0].createdAt, '변환 후:', dayjs(res.content[0].createdAt).valueOf());
   return res.content.map(note => ({
     noteId: note.noteId,
     memberId: note.memberId,
@@ -12,7 +12,8 @@ export const adaptNotesForSidebar = (
     pointX: note.pointX ?? 0,
     pointY: note.pointY ?? 0,
     role: note.role,
-    createdAt: note.createdAt,
-    updatedAt: note.updatedAt,
+    createdAt: dayjs(note.createdAt).valueOf(),
+    updatedAt: dayjs(note.updatedAt).valueOf(),
+
   }));
 };
