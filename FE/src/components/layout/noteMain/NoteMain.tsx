75d2ef8aa3c8bcb4954aab43bda0b4322ee89aa7
Swimming_Yang <ysw1mst@gmail.com>
2026-02-05 13:26:02 +0900

    faet : editToastMessage
    1. AI리뷰블록의 크기를 일치시켰습니다.

--- FE/src/components/layout/noteMain/NoteMain.tsx
+++ FE/src/components/layout/noteMain/NoteMain.tsx
@@ -161,13 +161,7 @@ const NoteMain: React.FC<NoteMainProps> = ({
 
     return (
         <div className="note-main-layout">
-            <NoteSummary
-                summary={summary}
-                summaryStyle={summaryStyle}
-                summaryUpdatedAt={summaryUpdatedAt}
-                isLoading={isSummaryLoading ?? false}
-                onGenerateSummary={onGenerateSummary ?? (() => { })}
-            />
+
             <div className="note-body-wrapper">
                 {/* External sidebar area removed */}
 
@@ -195,6 +189,13 @@ const NoteMain: React.FC<NoteMainProps> = ({
                                 />
                             </header>
                             <div className="note-content-area">
+                                <NoteSummary
+                                    summary={summary}
+                                    summaryStyle={summaryStyle}
+                                    summaryUpdatedAt={summaryUpdatedAt}
+                                    isLoading={isSummaryLoading ?? false}
+                                    onGenerateSummary={onGenerateSummary ?? (() => { })}
+                                />
                                 <Reorder.Group
                                     values={localBlocks}
                                     onReorder={(newOrder) => {
