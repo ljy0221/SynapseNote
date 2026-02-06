371f26c1910415472771e5ffd95597da1d898594
jimmy0524 <10jmin04@naver.com>
2026-02-07 01:37:24 +0900

    refactor: apply feedback

--- FE/electron/preload.ts
+++ FE/electron/preload.ts
@@ -31,7 +31,9 @@ contextBridge.exposeInMainWorld('electronAPI', {
   },
   removeDeepLinkListener: (callback: (event: any, url: string) => void) => {
     ipcRenderer.removeListener('deep-link-url', callback);
-  }
+  },
+  // [New] 대기 중인 Deep Link 조회 (Pull 방식)
+  getDeepLink: () => ipcRenderer.invoke('docker:get-deep-link'),
 })
 
 // Docker API 노출
